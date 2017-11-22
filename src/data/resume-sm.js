badd6b934eb1bd537cd18a4124c3f7c7b7c830a6
Valentin Vinogradskiy <vvinogradskiy@dataart.com>
2017-11-22 12:45:08 +0100

    HR-VIEW applied viewport fixes

---
+++ src/data/resume-sm.js
@@ -0,0 +1,180 @@
+export default {
+    author: 'vvinogradskiy',
+    name: 'Valentyn Vynogradskyi',
+    search: '@vynogradskyi',
+    position: 'Senior Front-End Developer',
+    address: {
+        country: 'Poland',
+        city: 'Wroclaw',
+        street: 'Konduktorska 8/3',
+        zip: '52-130'
+    },
+    telephone: '+48-737-930-864',
+    email: 'vinogradskiy.v.a@gmail.com',
+    sections: {
+        aboutMe: {
+            title: 'ABOUT ME',
+            text: `Hello, I’m Valentyn. It’s really hard to pronounce, so you can use Val, Valentin or Valek. My current place of living is Wroclaw, but my home city - Kharkiv, Ukraine.
+                    I’m a straight open person. Really goal focused. Have a strong experience in front end development and programming itself. I have started my career as a back-end developer,
+                     and some time ago moved to a front-end, because was charmed by flexibility and transparency of the JavaScript. But you always have to pay. In this case currency is time 
+                     for self-education. So am I. But the time spent on learning for being up to date really worth it. I like playing football, rock climbing, computer gaming.This is my CV.
+                      Hope you enjoy!`
+        },
+        experience: [
+            {
+                position: "Senior Front-End Developer",
+                company: 'Data-Art',
+                date: 'since Aug 2015',
+                city: 'Wroclaw',
+                description: `Was hired as a fullstask developer, but after half a year was requalified to pure front-end developer. And I like it! Projects:`,
+                points: [
+                    'Logistics portal with infinite number of different features. (Asp MVC 4, MS SQL, Javascript, jQuery)',
+                    'Art portal: admin view for users. (React+Redux, ES6, Webpack)',
+                    'Art portal: ArtFair site. (React, React-Router v4, CRA)',
+                    'Art portal: bug fix on a number of other projects'
+                ]
+            },
+            {
+                position: "Software Developer",
+                company: 'SMT Software',
+                date: 'Jan 2014 - Aug 2015',
+                city: 'Wroclaw',
+                description: 'I was working for a Credit Suisse as a contractor. Leading full stack development of the unauthorized trading control system.' +
+                ' The tech: Angular.js, .Net (Web Api), MS SQL, MS SSRS, Console app, Dapper. Projects:',
+                points: [
+                    'Report system. Tech:  DB + SSRS + C#',
+                    'Unauthorized trading control system. (Estimates, Architecture, Development, Leading, Planning)'
+                ]
+            },
+            {
+                position: "Software Developer",
+                company: 'Dev-Pro',
+                date: 'Nov 2012 - Jan 2014',
+                city: 'Kharkiv',
+                description: `At Dev-Pro i used to work on the Search/Cache system for custom mobile clients of the MS Dynamics CRM. Really challenging project with high
+                code quality requirements. Was responsible for:`,
+                points: [
+                    'New feature development / estimate',
+                    'Bug fix / deploy / code review / environment issue',
+                    'Front end side (there was a health notification admin web site)',
+                    'Also i was responsible for a good mood in our room',
+                    'Dialogue with client'
+                ]
+            },
+            {
+                position: "Junior Software Developer",
+                company: 'Intetics',
+                date: 'Mar 2011 - Nov 2012',
+                city: 'Kharkiv',
+                description: 'As a junior developer i was most of time overseen by more experienced developers and was working on internal projects most of time. ' +
+                'There were multiple project i was working on, during time spent in Intetics:',
+                points: [
+                    'Internal HR portal, for resource management. (FE, BE)',
+                    'Small web-store. (BE)',
+                    'Web forms application for converting .csv files to a valid Arc Gis maps, with some extra informations.'
+                ]
+            }
+        ],
+        coding: {
+            title: 'Coding',
+            points: [
+                {
+                    title: 'Javascript / HTML / CSS',
+                    level: 'Senior'
+                },
+                {
+                    title: 'C#',
+                    level: 'Middle'
+                },
+                {
+                    title: 'MS SQL',
+                    level: 'Middle'
+                },
+                {
+                    title: 'Java, python, C',
+                    level: 'Junior'
+                }
+            ]
+        },
+        techstack: {
+            title: 'Tech Stack',
+            points: [
+                'React.js, Angular.js, jQuery, Knockout.js, ES6',
+                'Redux, Flux, MVC, MVVM',
+                'RabbitMQ, Node.js, ElasticSearch',
+                'ASP.NET MVC 1-6, Web Api, Core',
+                'MongoDB, CouchDB, MS SQL'
+            ]
+        },
+        personal: {
+            title: 'Personal',
+            points: [
+                'Responsive, Reliable, Focused',
+                '“Finish project” oriented',
+                'Code quality oriented',
+                'Self learning, keeping up myself up-to-date',
+                'Open to help, but don’t like 1hr coffee breaks',
+                'Easy excited, often in a good mood',
+            ]
+        },
+        social: {
+            title: 'Social Media',
+            points: [
+                {
+                    site: 'linkedin',
+                    value: 'vynogradskyi',
+                    url: 'https://www.linkedin.com/in/vynogradskyi/'
+
+                },
+                {
+                    site: 'skype',
+                    value: 'valek19891',
+                    nohref: true
+                },
+                {
+                    site: 'facebook',
+                    value: 'vynogradskyi',
+                    url: 'https://www.facebook.com/vynogradskyi'
+                },
+                {
+                    site: 'twitter',
+                    value: 'vynogradskyi',
+                    url: 'https://twitter.com/vynogradskyi'
+                },
+                {
+                    site: 'email',
+                    value: 'vinogradskiy.v.a@gmail.com',
+                    url: 'mailto:vinogradskiy.v.a@gmail.com',
+                    noTarget: true
+                },
+                {
+                    site: 'instagram',
+                    value: 'vynogradskyi',
+                    url: 'https://www.instagram.com/vynogradskyi/'
+                },
+                {
+                    site: 'github',
+                    value: 'vynogradskyi',
+                    url: 'https://github.com/vynogradskyi'
+                }
+            ]
+        },
+        education: {
+            title: 'Computer Science',
+            university: 'National Aerospace University',
+            city: 'Kharkiv',
+            dates: '2006 - 2012',
+            level: 'Master Degree'
+        },
+        hobbies: {
+            title: 'Hobbies',
+            points: [
+                'football',
+                'programming',
+                'table tennis',
+                'package tours'
+            ]
+        }
+    }
+
+}
